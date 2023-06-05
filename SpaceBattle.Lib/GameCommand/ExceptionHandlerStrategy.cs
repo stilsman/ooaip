e94408544383bed7a5a18e23b9c2308e154cdcb2
Sergei <sergeipikalov.2003@gmail.com>
2023-06-06 04:24:49 +0600

    excStrat

---
+++ SpaceBattle.Lib/GameCommand/ExceptionHandlerStrategy.cs
@@ -0,0 +1,32 @@
+using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class ExceptionHandlerStrategy : IStrategy
+{
+    public object RunStrategy(params object[] args)
+    {
+        ICommand cmd = (ICommand)args[0];
+        Exception exc = (Exception)args[1];
+
+        int cmdHash = cmd.GetType().GetHashCode();
+        int excHash = exc.GetType().GetHashCode();
+
+        var excTree = IoC.Resolve<Dictionary<int, Dictionary<int, IStrategy>>>("Exception.GetTree");
+
+        Dictionary<int, IStrategy>? excSubTree;
+
+        if (!excTree.TryGetValue(cmdHash, out excSubTree))
+        {
+            excSubTree = IoC.Resolve<Dictionary<int, IStrategy>>("Exception.NotFoundCommandSubTree");
+        }
+
+        IStrategy? excHandler;
+
+        if (!excSubTree.TryGetValue(excHash, out excHandler))
+        {
+            return IoC.Resolve<IStrategy>("Exception.NotFoundExceptionHandler");
+        }
+        return excHandler;
+    }
+}
