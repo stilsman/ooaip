e0674ce8016c192f3261c8b3e4ac39be172e0f28
Konstantin Zaitsev <wrsss0040@gmail.com>
2023-06-06 22:15:15 +0600

    gogo

---
+++ corewcf/WebApi.cs
@@ -0,0 +1,21 @@
+﻿using CoreWCF;
+using Hwdtech;
+
+namespace WCF;
+
+[ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+internal class WebApi : IWebApi
+{
+    public void GetOperationMessage(MessageContract param)
+    {
+        try
+        {
+            var threadId = IoC.Resolve<string>("GetThreadID", param.gameId);
+            IoC.Resolve<SpaceBattle.Lib.ICommand>("SendCommand", threadId, IoC.Resolve<SpaceBattle.Lib.ICommand>("CreateCommandFromMessage", param)).Execute();
+        }
+        catch (System.Exception e)
+        {
+            System.Console.WriteLine($"\n{e}\n");
+        }
+    }
+}
